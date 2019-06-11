e7e0529bb944e0c804daa6329246934ab8960f0c
Dasul Lee <nicoledasullee@gmail.com>
2019-06-10 19:21:14 -0700

    Completed setting up webpack-dev-server and babel.

---
+++ src/add_to_dom.js
@@ -0,0 +1,8 @@
+function addToDom(tagName, text){
+    const newElement = document.createElement(tagName);
+    newElement.innerText = text;
+    const root = document.getElementById('root');
+    root.appendChild(newElement);
+}
+
+export default addToDom;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,37 @@
-var $ = require("jquery")
+import atd  from './add_to_dom';
 
-var root = $('.root')
+function delayed(){
+    console.log('1. Delayed function called');
+    const myPromise = new Promise(function(resolve, reject){
+        console.log('2. Promise callback called');
+        setTimeout(function(){
+            console.log('3. SetTimeout callback called')
+            resolve('This is the info you requested!');
+            // reject(new Error('Something went wrong!'));
+        },3000);
+    });
+    console.log('4. End of delayed function, returning promise');
+    return myPromise;
+}
 
-root.html('Im Cool')
+async function handlePromiseNew(){
+    const resp = await delayed();
+
+    console.log('Delayed Response:', resp);
+}
+
+function handlePromiseOld(){
+    const delayedString = delayed().then(function(resp){
+        console.log('Then Callback called');
+        console.log('Delayed Response:', resp);
+    }).catch((err)=>{
+        console.log('Catch called:', err.message);
+    });
+    //the order of .then and .catch does not matter
+    //we could just use one or the other (do not need both)
+
+    console.log('Delayed String:',delayedString);
+}
+
+// handlePromiseOld();
+handlePromiseNew();
